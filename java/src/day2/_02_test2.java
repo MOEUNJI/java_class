97033ef0483128e183c22911c77559014d188f36
Admin <Admin@F-9>
2025-02-03 19:22:11 +0900

    day1test

--- java/src/day2/_02_test2.java
+++ java/src/day2/_02_test2.java
@@ -1,10 +1,10 @@
-package day1;
+package day2;
 
-public class _01_HelloWorld {
+public class _02_test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("TEst");
 	}
 
 }
