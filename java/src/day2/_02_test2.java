10eddc069d4d87c984aa0fa91312893bee89a74c
Admin <Admin@F-9>
2025-02-03 19:27:14 +0900

    day1 testestest

--- java/src/day2/_02_test2.java
+++ java/src/day2/_02_test2.java
@@ -5,7 +5,7 @@ public class _02_test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TEst");
-		System.out.println("test")
+		System.out.println("test");
 	}
 
 }
